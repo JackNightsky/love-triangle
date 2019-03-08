52119f2c8754371d6559f5e29092fa072c4c90c8
JackNightsky <jack.shapyalevich@gmail.com>
2019-03-08 03:01:43 +0300

    Create code. Complite. 100 pass

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+  	if (i+1 != preferences[i]) {
+  	    if (preferences[preferences[preferences[i]-1]-1] == i+1) {
+  	    	count++;
+  	    }
+    }
+   //возврат значения из функции для проверки количества треугольников
+  }
+  count = count / 3
+  return count;
 };
